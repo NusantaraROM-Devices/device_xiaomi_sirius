a5329020b053b87bd04bf4e0209d12f4eee54b98
Arian <arian.kulmer@web.de>
2020-09-22 20:06:14 +0200

    sirius: Build new aidl qti power HAL
    Change-Id: I4bdea354e54d34d0dfe9b811e228fc6f8762d2ed

--- sirius.mk
+++ sirius.mk
@@ -295,7 +295,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service.xiaomi_sirius
+    android.hardware.power-service-qti
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
