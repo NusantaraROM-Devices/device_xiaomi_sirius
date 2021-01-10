c088df9512d661e8f4d3b1eac6ae34a94bb7527b
Dimas Yudha Pratama <dimasyudha@domainm.my.id>
2021-01-10 14:38:42 +0700

    sirius: Nad-ify

--- nad_sirius.mk
+++ nad_sirius.mk
@@ -7,16 +7,20 @@
 $(call inherit-product, device/xiaomi/sirius/sirius.mk)
 
 # Inherit some common Mokee stuff.
-$(call inherit-product, vendor/mokee/config/common_full_phone.mk)
+NAD_BUILD_TYPE := OFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+WITH_GAPPS := true
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := mokee_sirius
+PRODUCT_NAME := nad_sirius
 PRODUCT_DEVICE := sirius
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 8 SE
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/sirius/sirius:10/QKQ1.190828.002/V12.0.2.0.QEBCNXM:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1D.210105.003/7005430:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="sirius-user 10 QKQ1.190828.002 V12.0.2.0.QEBCNXM release-keys" \
