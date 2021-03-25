8d4089f939d5e04ebabdb99209e5f272d7bb006a
Edwin Wong <edwinwong@google.com>
2021-03-25 14:36:24 -0700

    sirius: Update clearkey plugin to drm HAL v1.4
    Bug: 182525516
    Change-Id: I406b7d3c2921d4698535421894c6eb3785eb1c0d
    Signed-off-by: Dimas Yudha Pratama <fsdimasyudha@gmail.com>

--- sirius.mk
+++ sirius.mk
@@ -176,7 +176,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.4-service.clearkey
 
 $ Gatekeeper
 PRODUCT_PACKAGES += \
