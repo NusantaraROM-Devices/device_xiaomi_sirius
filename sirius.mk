86dfc11127dae1f517ea643f1bdd9a0aa70631fe
Dimas Yudha Pratama <fsdimasyudha@gmail.com>
2021-10-16 20:20:46 +0700

    sirius: Nuke Trust by LOS
    Signed-off-by: Dimas Yudha Pratama <fsdimasyudha@gmail.com>

--- sirius.mk
+++ sirius.mk
@@ -382,10 +382,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-service \
     thermal.sdm710
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.mokee.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
